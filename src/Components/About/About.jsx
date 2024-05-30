c0ff66b1bbd488cb85a7f657803b088cdcce79ed
niteshagarwal-coder <niteshagarwal901@gmail.com>
2024-05-30 17:57:07 +0530

    react project

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = ({setPlayState}) => {
+  return (
+    <div className='about'>
+      <div className="about-left">
+        <img src={about_img} alt="" className='about-img'/>
+        <img src={play_icon} alt="" className='play-icon' onClick={()=>{setPlayState(true)}}/>
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>Embark on a transformative educational journey</p>
+        <p>With a focus on innovation, hands-on learning</p>
+        <p>Whether you aspire to become a teacher</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
